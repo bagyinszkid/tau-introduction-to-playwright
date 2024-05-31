b7acf6700399a8e854e526e94b2ab3dbf3e4d34d
bagyinszkid <123751730+bagyinszki@users.noreply.github.com>
2024-05-31 14:46:11 +0200

    'a space added to config file'

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,6 +55,7 @@ export default defineConfig({
     // viewport: { width: 1280, height: 720 },
     // video: 'on-first-retry',
   },
+  
     // timeout: 30000, //https://playwright.dev/docs/test-timeouts
     // expect: {
       /**
